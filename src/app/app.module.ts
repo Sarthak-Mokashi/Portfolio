271268ec4fba7660e4108f957d7db396cdfebab5
Sarthak-Mokashi <sarthakmokashi41@gmail.com>
2021-03-19 21:22:55 +0530

    Blogs Section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
 import { SocialmediaComponent } from './socialmedia/socialmedia.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { AboutComponent } from './about/about.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { AboutComponent } from './about/about.component';
     HeaderComponent,
     SocialmediaComponent,
     NavigationComponent,
-    AboutComponent
+    AboutComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule,
